ae9f20426bea4921517401fcc4319ce163cb3e17
tmtimur <t.tastanov@gmail.com>
2026-01-15 16:50:47 +0500

    Bug Fixes. Home block editing and creating functions added.

--- main/urls.py
+++ main/urls.py
@@ -1,23 +1,13 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import HomeView
+from .views import HomeView, HomeBlockCreateView, HomeBlockUpdateView
 
 app_name = "main"
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
-
-    path(
-        "login/",
-        auth_views.LoginView.as_view(
-            template_name="registration/login.html"
-        ),
-        name="login"
-    ),
-
-    path(
-        "logout/",
-        auth_views.LogoutView.as_view(),
-        name="logout"
-    )
+    path("blocks/create/", HomeBlockCreateView.as_view(), name="block_create"),
+    path("blocks/<int:pk>/edit/", HomeBlockUpdateView.as_view(), name="block_edit"),
+    path("login/", auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout")
 ]
--- main/views.py
+++ main/views.py
@@ -1,8 +1,30 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView, UpdateView
 from .models import HomeBlock
 from article.models import Article
 from project.models import Project
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import HomeBlock
+from .forms import HomeBlockForm
 
+
+class HomeBlockCreateView(LoginRequiredMixin, CreateView):
+    model = HomeBlock
+    form_class = HomeBlockForm
+    template_name = "main/homeblock_create.html"
+
+    def get_success_url(self):
+        return reverse_lazy("main:home")
+
+
+class HomeBlockUpdateView(LoginRequiredMixin, UpdateView):
+    model = HomeBlock
+    form_class = HomeBlockForm
+    template_name = "main/homeblock_edit.html"
+
+    def get_success_url(self):
+        return reverse_lazy("main:home")
+    
 
 class HomeView(TemplateView):
     template_name = "main/home.html"
@@ -11,6 +33,6 @@ class HomeView(TemplateView):
         context = super().get_context_data(**kwargs)
         context["blocks"] = HomeBlock.objects.filter(is_active=True)
         context["articles"] = Article.objects.order_by("-published_at").filter(is_published=True)[:3]
-        context["projects"] = Project.objects.order_by("-created_at").all()[:6]
+        context["projects"] = Project.objects.order_by("-created_at").all()[:9]
         context["page_title"] = "Главная"
         return context
