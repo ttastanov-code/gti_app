81b3865b70f55a7dfaa0552fbb78c45a98f82a01
tmtimur <t.tastanov@gmail.com>
2026-01-19 16:37:13 +0500

    feedparser was added to the library. Context processor was deleted.

--- main/context_processors.py
+++
@@ -1,8 +0,0 @@
-from django.conf import settings
-
-def analytics(request):
-    return {
-        "YANDEX_METRIKA_ID": settings.YANDEX_METRIKA_ID,
-        "GOOGLE_ANALYTICS_ID": settings.GOOGLE_ANALYTICS_ID,
-        "debug": settings.DEBUG,
-    }
